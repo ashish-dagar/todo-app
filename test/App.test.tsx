f85c8ffdbb4e728a190184a86626b69cf07b5a80
ashish-dagar <ashish.93dagar@gmail.com>
2024-05-06 12:08:38 +0530

    Updated Test Cases coverage

--- test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../src/App';
-
-test('renders app component', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Create Your ToDo's/i);
-  expect(linkElement).toBeInTheDocument();
-});
